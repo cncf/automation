291026d37c32d6799bcd1c3e073369b8f54cb880
Koray Oksay <koray.oksay@gmail.com>
2025-06-18 14:31:26 +0300

    Add qemuargs
    Signed-off-by: Koray Oksay <koray.oksay@gmail.com>

--- ci/gha-runner-vm/main.go
+++ ci/gha-runner-vm/main.go
@@ -360,7 +360,12 @@ variable architecture {
 }
 
 source "qemu" "img" {
-	qemu_binary          = var.architecture == "amd64" ? "/usr/bin/qemu-system-x86_64" : "/usr/bin/qemu-system-aarch64"
+	qemu_binary          = var.architecture == "arm64" ? "/usr/bin/qemu-system-aarch64" : "/usr/bin/qemu-system-x86_64"
+	qemuargs             = var.architecture == "arm64" ? [
+								["-machine", "virt"],
+								["-cpu", "cortex-a57"],
+								["-bios", "/usr/share/qemu-efi-aarch64/QEMU_EFI.fd"]
+							] : []
 	vm_name              = "image.raw"
 	cd_files             = ["./cloud-init/*"]
 	cd_label             = "cidata"
