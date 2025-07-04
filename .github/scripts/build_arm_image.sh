d295ef7f546d542f35d49dc626770812dbb1dbf4
Koray Oksay <koray.oksay@gmail.com>
2025-07-04 12:13:20 +0300

    Add build script to the workflow
    Signed-off-by: Koray Oksay <koray.oksay@gmail.com>

---
+++ .github/scripts/build_arm_image.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export OCI_CLI_USER=$1
+export OCI_CLI_TENANCY=$2
+export OCI_CLI_FINGERPRINT=$3
+export OCI_CLI_KEY_CONTENT=$4
+export OCI_CLI_REGION=$5
+export OCI_INSTANCE_ID=$6
+
+sudo apt update
+sudo apt install -y qemu-system-x86 xorriso qemu-system-arm qemu-efi-aarch64 git golang zip
+
+echo 'KERNEL=="kvm", GROUP="kvm", MODE="0666", OPTIONS+="static_node=kvm"' | sudo tee /etc/udev/rules.d/99-kvm4all.rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger --name-match=kvm
+sudo kvm-ok
+
+curl -LO https://releases.hashicorp.com/packer/1.12.0/packer_1.12.0_linux_arm64.zip
+unzip packer_1.12.0_linux_arm64.zip
+sudo mv packer /usr/local/bin/
+rm packer_1.12.0_linux_arm64.zip
+packer plugin install github.com/hashicorp/oracle
+packer plugins install github.com/hashicorp/qemu
+packer plugins install github.com/hashicorp/azure
+
+curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh
+chmod +x install.sh
+./install.sh --accept-all-defaults
+export PATH=$PATH:$HOME/bin
+
+oci compute image list \
+  --compartment-id ocid1.compartment.oc1..aaaaaaaa22icap66vxktktubjlhf6oxvfhev6n7udgje2chahyrtq65ga63a \
+  --operating-system runner-images \
+  --operating-system-version 123456
+
+git clone https://github.com/cncf/automation
+cd automation/ci/gha-runner-vm
+
+PACKER_LOG=1 go run main.go \
+  --isoURL https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-arm64.img \
+  --arch arm64
