bfcf4b03901b3c821d71a8b9c6f8350dcdd4e5ec
Jeffrey Sica <me@jeefy.dev>
2025-07-28 10:44:36 -0500

    work on filepath matching
    Signed-off-by: Jeffrey Sica <me@jeefy.dev>

--- utilities/labeler/main.go
+++ utilities/labeler/main.go
@@ -91,15 +91,11 @@ func githubClient() (*github.Client, error) {
 	return github.NewClient(oauth2.NewClient(context.Background(), ts)), nil
 }
 
-var (
-	changedFiles = flag.String("changed-files", "", "Comma-separated list of changed files")
-)
-
 func main() {
 	flag.Parse()
 
 	if len(flag.Args()) < 5 {
-		fmt.Println("Usage: labeler [flags] <labels_url> <owner> <repo> <issue_number> <comment_body>")
+		fmt.Println("Usage: labeler [flags] <labels_url> <owner> <repo> <issue_number> <comment_body> <changed_files>")
 		os.Exit(1)
 	}
 	labelsURL := flag.Arg(0)
@@ -107,6 +103,7 @@ func main() {
 	repo := flag.Arg(2)
 	issueNum := flag.Arg(3)
 	commentBody := flag.Arg(4)
+	changedFiles := flag.Arg(5)
 
 	if labelsURL == "" {
 		log.Fatal("labels URL not set")
@@ -138,10 +135,12 @@ func main() {
 	}
 
 	// Parse changed files if provided
+	log.Printf("Changed files: %s", changedFiles)
 	var files []string
-	if *changedFiles != "" {
-		files = strings.Split(*changedFiles, ",")
+	if changedFiles != "" {
+		files = strings.Split(changedFiles, ",")
 	}
+	log.Printf("Parsed changed files: %v", files)
 
 	issue, _, err := client.Issues.Get(ctx, owner, repo, toInt(issueNum))
 	if err != nil {
