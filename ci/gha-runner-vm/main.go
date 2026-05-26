2e48a62e192b597507fda6d31ca0a0a12ffce2dd
Koray Oksay <koray.oksay@gmail.com>
2026-05-26 11:34:40 +0300

    take haskell GHcup out of the software report, revert previous env changes
    Signed-off-by: Koray Oksay <koray.oksay@gmail.com>

--- ci/gha-runner-vm/main.go
+++ ci/gha-runner-vm/main.go
@@ -138,49 +138,48 @@ func run(cmd *cobra.Command, argv []string) error {
 	baseDir := strings.Split(filename, "/")[0]
 	installRunnerPackage(baseDir)
 
-	//temp for the maven error, probably will be fixed on the next action-runner release
-	replaceArmPackageLinks(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "\"maven\": \"3.9.13\"", "\"maven\": \"3.9.14\"")
+	updatePackerConfig(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", ".*Get-GHCupVersion.*", "")
 
 	if args.arch == "arm64" {
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-azcopy.sh", "https://aka.ms/downloadazcopy-v10-linux", "https://github.com/Azure/azure-storage-azcopy/releases/download/v10.29.1/azcopy_linux_arm64_10.29.1.tar.gz")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-runner-package.sh", "actions-runner-linux-x64", "actions-runner-linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-bicep.sh", "linux-x64", "linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-julia.sh", "x86_64", "aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-miniconda.sh", "x86_64", "aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-aws-tools.sh", "awscli-exe-linux-x86_64", "awscli-exe-linux-aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-aws-tools.sh", "aws-sam-cli-linux-x86_64", "aws-sam-cli-linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-aws-tools.sh", "ubuntu_64bit", "ubuntu_arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-cmake.sh", "x86_64", "aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-pulumi.sh", "linux-x64", "linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-dotnetcore-sdk.sh", "linux-x64", "linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-firefox.sh", "linux64.tar.gz", "linux-aarch64.tar.gz")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-swift.sh", "\\$\\(lsb_release -rs\\)", "24.04-aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-microsoft-edge.sh", "arch=amd64", "arch=arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-kubernetes-tools.sh", "linux-amd64", "linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-oras-cli.sh", "linux_amd64", "linux_arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-yq.sh", "linux_amd64", "linux_arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-docker.sh", "amd64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-packer.sh", "amd64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-terraform.sh", "amd64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-aliyun-cli.sh", "amd64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-github-cli.sh", "amd64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-java-tools.sh", "amd64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-pypy.sh", "x64", "aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-codeql-bundle.sh", "/x64", "/arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/install-ninja.sh", "ninja-linux.zip", "ninja-linux-aarch64.zip")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/configure-dpkg.sh", "wget .*", "wget http://launchpadlibrarian.net/723810004/libicu74_74.2-1ubuntu3_arm64.deb")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/configure-dpkg.sh", "libicu70_70.1-2_amd64.deb", "libicu74_74.2-1ubuntu3_arm64.deb")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/build/configure-dpkg.sh", "EXPECTED_LIBICU_SHA512=.*", "EXPECTED_LIBICU_SHA512=f5bc20c081d5dc6642a066052e69982702cf4b8638f77719567f7f30f622aae59ec1c23cb17842532c141768460369c176ad079e4ed22d6f4436f4ad86f30f79")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/docs-gen/SoftwareReport.CachedTools.psm1", "x64", "aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/docs-gen/SoftwareReport.Tools.psm1", "x64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", "Import-Module \\(Join-Path \\$PSScriptRoot \"SoftwareReport.Browsers.psm1\"\\) -DisableNameChecking", "")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", "# Browsers and Drivers\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*", "")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", "# Environment variables\n.*", "")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/scripts/tests/Browsers.Tests.ps1", "Describe \"Chrome\"(.|\n)*", "")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "linux-amd64", "linux-arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "linux-x86_64", "linux-aarch64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "x64", "arm64")
-		replaceArmPackageLinks(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "\"PyPy\",\n            \"arch\": \"arm64\"", "\"PyPy\",\n            \"arch\": \"aarch64\"")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-azcopy.sh", "https://aka.ms/downloadazcopy-v10-linux", "https://github.com/Azure/azure-storage-azcopy/releases/download/v10.29.1/azcopy_linux_arm64_10.29.1.tar.gz")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-runner-package.sh", "actions-runner-linux-x64", "actions-runner-linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-bicep.sh", "linux-x64", "linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-julia.sh", "x86_64", "aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-miniconda.sh", "x86_64", "aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-aws-tools.sh", "awscli-exe-linux-x86_64", "awscli-exe-linux-aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-aws-tools.sh", "aws-sam-cli-linux-x86_64", "aws-sam-cli-linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-aws-tools.sh", "ubuntu_64bit", "ubuntu_arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-cmake.sh", "x86_64", "aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-pulumi.sh", "linux-x64", "linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-dotnetcore-sdk.sh", "linux-x64", "linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-firefox.sh", "linux64.tar.gz", "linux-aarch64.tar.gz")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-swift.sh", "\\$\\(lsb_release -rs\\)", "24.04-aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-microsoft-edge.sh", "arch=amd64", "arch=arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-kubernetes-tools.sh", "linux-amd64", "linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-oras-cli.sh", "linux_amd64", "linux_arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-yq.sh", "linux_amd64", "linux_arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-docker.sh", "amd64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-packer.sh", "amd64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-terraform.sh", "amd64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-aliyun-cli.sh", "amd64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-github-cli.sh", "amd64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-java-tools.sh", "amd64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-pypy.sh", "x64", "aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-codeql-bundle.sh", "/x64", "/arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/install-ninja.sh", "ninja-linux.zip", "ninja-linux-aarch64.zip")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/configure-dpkg.sh", "wget .*", "wget http://launchpadlibrarian.net/723810004/libicu74_74.2-1ubuntu3_arm64.deb")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/configure-dpkg.sh", "libicu70_70.1-2_amd64.deb", "libicu74_74.2-1ubuntu3_arm64.deb")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/build/configure-dpkg.sh", "EXPECTED_LIBICU_SHA512=.*", "EXPECTED_LIBICU_SHA512=f5bc20c081d5dc6642a066052e69982702cf4b8638f77719567f7f30f622aae59ec1c23cb17842532c141768460369c176ad079e4ed22d6f4436f4ad86f30f79")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/docs-gen/SoftwareReport.CachedTools.psm1", "x64", "aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/docs-gen/SoftwareReport.Tools.psm1", "x64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", "Import-Module \\(Join-Path \\$PSScriptRoot \"SoftwareReport.Browsers.psm1\"\\) -DisableNameChecking", "")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", "# Browsers and Drivers\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*", "")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1", "# Environment variables\n.*", "")
+		updatePackerConfig(baseDir, "/images/ubuntu/scripts/tests/Browsers.Tests.ps1", "Describe \"Chrome\"(.|\n)*", "")
+		updatePackerConfig(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "linux-amd64", "linux-arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "linux-x86_64", "linux-aarch64")
+		updatePackerConfig(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "x64", "arm64")
+		updatePackerConfig(baseDir, "/images/ubuntu/toolsets/toolset-2404.json", "\"PyPy\",\n            \"arch\": \"arm64\"", "\"PyPy\",\n            \"arch\": \"aarch64\"")
 	}
 
 	command := exec.Command("packer", "build", "-var", "architecture="+args.arch, newFile)
@@ -412,7 +411,7 @@ func imageExists(imageName, imageVersion string) (bool, error) {
 	return true, nil
 }
 
-func replaceArmPackageLinks(baseDir string, filename string, searchString string, replaceString string) (string, error) {
+func updatePackerConfig(baseDir string, filename string, searchString string, replaceString string) (string, error) {
 	scriptName := baseDir + filename
 	err := replaceInFileRegex(scriptName, map[*regexp.Regexp]string{
 		regexp.MustCompile(searchString): replaceString,
@@ -751,7 +750,5 @@ build {
 
 	replacements[`destination = "${path.root}/../software-report.json"`] = `only = ["azure-arm.build_image"]
     destination = "${path.root}/../software-report.json"`
-
-	replacements[`inline           = ["pwsh -File ${var.image_folder}/SoftwareReport/Generate-SoftwareReport.ps1 -OutputDirectory ${var.image_folder}", "pwsh -File ${var.image_folder}/tests/RunAll-Tests.ps1 -OutputDirectory ${var.image_folder}"]`] = `inline           = ["export PATH=/usr/local/.ghcup/bin:$PATH && pwsh -File ${var.image_folder}/SoftwareReport/Generate-SoftwareReport.ps1 -OutputDirectory ${var.image_folder}", "export PATH=/usr/local/.ghcup/bin:$PATH && pwsh -File ${var.image_folder}/tests/RunAll-Tests.ps1 -OutputDirectory ${var.image_folder}"]`
 
 }
